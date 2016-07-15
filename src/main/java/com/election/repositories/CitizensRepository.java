676f61870d11c2a00bad8316cce3f8b1c3f8921d
kamil <Kamil_wit1@wp.pl>
2016-07-15 21:39:19 +0200

    Revert "Dodanie klasy, repository i serwisu Citizens oraz mała modyfikacja klasy Loader"
    This reverts commit 0eed4a115930bb41fe47dd24b522a5a32571b7a9.

--- src/main/java/com/election/repositories/CitizensRepository.java
+++
@@ -1,8 +0,0 @@
-package com.election.repositories;
-import org.springframework.data.repository.CrudRepository;
-
-import com.election.domain.Citizens;
-
-public interface CitizensRepository extends CrudRepository<Citizens, Integer> {
-	int countByPeselAndIdnumber(String pesel, String idnumber);
-}
